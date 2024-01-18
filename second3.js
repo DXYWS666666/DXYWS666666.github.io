728860ca711ba9a434b83650a961be18d7d9b6ab
DXYWS666666 <135585062+DXYWS666666@users.noreply.github.com>
2024-01-19 00:50:46 +0800

    Add files via upload

---
+++ second3.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = ['p1.jpg', 'p2.jpg','p3.jpg'];
+    let currentSlide = 0;
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        document.getElementById('slide').src = slides[currentSlide];
+    }
+
+    setInterval(nextSlide, 3000); // 每3秒切换照片
+});
